b57ea881266df45a312cb431dec09a3503d39688
ginedou0315 <ginelle.doubek@gmail.com>
2024-11-30 17:11:58 +0100

    added Js for city search

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("hello world");
+function searchInput(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#city-element");
+  cityElement.innerHTML = cityInput.value;
+}
+
+let cityInputValue = document.querySelector("#search-input-form");
+cityInputValue.addEventListener("submit", searchInput);
