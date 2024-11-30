383cd5c9583270c268b43d50badfac60658b080c
ginedou0315 <ginelle.doubek@gmail.com>
2024-11-30 17:49:38 +0100

    added JS for date, day and time...

--- src/index.js
+++ src/index.js
@@ -7,3 +7,42 @@ function searchInput(event) {
 
 let cityInputValue = document.querySelector("#search-input-form");
 cityInputValue.addEventListener("submit", searchInput);
+
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let currenttime = new Date();
+let currentMonth = months[currenttime.getMonth()];
+let currentDay = days[currenttime.getDay()];
+let currentDate = `${currenttime.getDate()} ${currentMonth} ${currenttime.getFullYear()}`;
+let currentHours = currenttime.getHours();
+let currentMinutes = currenttime.getMinutes();
+
+let dateElement = document.querySelector("#date-element");
+dateElement.innerHTML = currentDate;
+
+let dayElement = document.querySelector("#day-input");
+dayElement.innerHTML = currentDay;
+
+let timeElement = document.querySelector("#time-input");
+timeElement.innerHTML = `${currentHours}:${currentMinutes}`;
